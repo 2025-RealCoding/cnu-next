83545b0559f086405c1d4a89a5cb9d5a51ac1c52
dolup <kimdu@o.cnu.ac.kr>
2025-06-15 00:41:29 +0900

    code change

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Header from "../../component/layout/Header";
 import Footer from "../../component/layout/Footer";
 import SearchInput from "../../component/search/SearchInput";
@@ -7,15 +6,20 @@ import ProductCart from "../../component/shopping/ProductCart";
 import { useUser } from "../../context/UserContext";
 import { useEffect } from "react";
 import { useSearch } from "../../context/SearchContext";
-
 export default function SearchHome() {
   const { user, setUser } = useUser();
   const { result } = useSearch();
-
   //  페이지 최초 렌더링 될 때, setUser로 이름 설정
   useEffect(() => {
     //  학번 + 이름 형태로 작성 (ex. 2025***** 내이름 )
     setUser({ name: "" });
+    setUser({
+    name: "202102604 권혁주",
+    age: 24,
+    userId: "chocohj124",
+    email: "chocohj124@gmail.com",
+    phoneNumber: "010-2829-5572",
+  });
   }, []);
 
   return (
