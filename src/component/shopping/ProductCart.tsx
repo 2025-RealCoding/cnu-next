35f62cf42b89308b3a14b345653a59b06098914d
dongmin-dev <dongminlee.0711@gmail.com>
2025-06-15 02:47:25 +0900

    feat(cart): Implement item removal from cart

--- src/component/shopping/ProductCart.tsx
+++ src/component/shopping/ProductCart.tsx
@@ -5,37 +5,43 @@ import { ProductItem } from "@/types/Product";
 import CartList from "./CartList";
 
 export default function ProductCart({ items }: { items: ProductItem[] }) {
-  const [cart, setCart] = useState<{ [id: string]: number }>({}); // {"88159814281" : 1}
-  const [showCart, setShowCart] = useState(false); //  과제 2.1
+  const [cart, setCart] = useState<{ [id: string]: number }>({});
+  const [showCart, setShowCart] = useState(false);
 
-  // 과제 2.1: cart 상태가 변경될 때마다 showCart 상태를 업데이트합니다.
   useEffect(() => {
-    // cart 객체에 상품이 하나 이상 있는지 확인합니다.
     const hasItemsInCart = Object.keys(cart).length > 0;
     setShowCart(hasItemsInCart);
   }, [cart]);
 
-  //  카트에 담기
   const handleAddToCart = (item: ProductItem, quantity: number) => {
     setCart((prev) => ({
       ...prev,
       [item.productId]: quantity,
     }));
 
+    // 로컬 스토리지에 아이템 저장
     localStorage.setItem(item.productId, quantity + "");
-    localStorage.getItem(item.productId);
   };
 
-  /* 과제 2-3: Cart 아이템 지우기 */
-  const handleRemoveFromCart = () => {};
+  /* 과제 2.3: Cart 아이템 지우기 */
+  const handleRemoveFromCart = (productId: string) => {
+    // 1. cart 상태 업데이트
+    setCart((prevCart) => {
+      // 이전 cart 상태를 복사
+      const newCart = { ...prevCart };
+      // 특정 productId를 가진 속성을 삭제
+      delete newCart[productId];
+      return newCart;
+    });
+
+    // 2. localStorage에서 아이템 삭제
+    localStorage.removeItem(productId);
+  };
 
   return (
     <div className="p-10">
-      {/* 상품 리스트 */}
       <ProductList items={items} onAddToCart={handleAddToCart} />
 
-      {/* 장바구니 */}
-      {/* 2.1. 조건부 카트 보이기: showCart가 true일 때만 CartList를 렌더링합니다. */}
       {showCart && (
         <CartList
           cart={cart}
