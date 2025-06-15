0c335d6607f9ae2632ffad7396ba9f2cd5b156d7
qqwertqwrtq1241 <127834169+imHay0ung@users.noreply.github.com>
2025-06-15 13:39:22 +0900

    Update ProductCart.tsx

--- src/component/shopping/ProductCart.tsx
+++ src/component/shopping/ProductCart.tsx
@@ -7,6 +7,11 @@ import CartList from "./CartList";
 export default function ProductCart({ items }: { items: ProductItem[] }) {
   const [cart, setCart] = useState<{ [id: string]: number }>({}); // {"88159814281" : 1}
   const [showCart, setShowCart] = useState(false); //  과제 2.1
+  useEffect(() => {
+    const hasItems = Object.keys(cart).length > 0;
+    setShowCart(hasItems);
+  }, [cart])
+
 
   //  카트에 담기
   const handleAddToCart = (item: ProductItem, quantity: number) => {
@@ -20,7 +25,17 @@ export default function ProductCart({ items }: { items: ProductItem[] }) {
   };
 
   /* 과제 2-3: Cart 아이템 지우기 */
-  const handleRemoveFromCart = () => {};
+  const handleRemoveFromCart = (productId: string) => {
+    setCart((prev) => {
+      const updated = Object.fromEntries(
+        Object.entries(prev).filter(([id]) => id !== productId)
+      );
+      return updated;
+    });
+
+    localStorage.removeItem(productId);
+  };
+
 
   return (
     <div className="p-10">
@@ -28,7 +43,13 @@ export default function ProductCart({ items }: { items: ProductItem[] }) {
       <ProductList items={items} onAddToCart={handleAddToCart} />
       {/* 장바구니 */}
       {/* 2.1. 조건부 카트 보이기: 카트에 담긴 상품이 없으면 카트가 보이지 않고, 카트에 담긴 물건이 있으면 카트가 보인다 */}
-      <CartList cart={cart} products={items} onRemove={handleRemoveFromCart} />
+      {showCart && (
+        <CartList
+          cart={cart}
+          products={items}
+          onRemove={handleRemoveFromCart}
+        />
+      )}
     </div>
   );
 }
